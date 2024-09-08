3c937f5d240ef463f62b4e1d9f2c63df3a3bc6bb
Syniah Peterson <122937585+syniahpeterson@users.noreply.github.com>
2024-09-08 10:36:20 -0400

    Committed Animated Search Bar

---
+++ Animated-Search-Bar/script.js
@@ -0,0 +1,6 @@
+const searchBarContainer = document.querySelector(".search-bar-container");
+const magnifier = document.querySelector(".magnifier");
+
+magnifier.addEventListener("click", () => {
+    searchBarContainer.classList.toggle("active");
+})
